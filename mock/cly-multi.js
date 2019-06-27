2dd704f6a2ce69fd42503aa06fed1c52c154e84b
huzhenhui <369103724@qq.com>
2019-06-27 18:21:40 +0800

    fix

--- mock/cly-multi.js
+++ mock/cly-multi.js
@@ -10,7 +10,7 @@ const broker = process.env.BROKER || '60.30.105.251';
 // const broker = process.env.BROKER || '47.102.200.126';
 const username = 'mos';
 const password = 'mos';
-const device_count = process.env.DEVICE_COUNT || 2;
+const device_count = process.env.DEVICE_COUNT || 500;
 
 // celiuyi topic
 const cly_topic_prefix = '/cly/up/';
@@ -19,34 +19,18 @@ const cly_topic_prefix2 = '/cly/down/';
 const cly_upload_sufix = '';
 const cly_download_sufix = '';
 
+// 模拟数据
+var cly_data = ['#UPLOAD,3,1,75,001,95.20000,青岛欧森测试,12345678,12345678,青岛港,柴油,0#,青岛',
+    '#UPLOAD,3,2,75,欧森海事技术服务有限公司,1,青岛欧森,1,标准样品,xxx,',
+    '#UPLOAD,3,3,56,2019/06/27 16:01,青岛欧森海事技术服务有限公司e'
+];
+
 let deviceArray = [];
 var deviceTopics = [];
 for (var i = 0; i < device_count; i++) {
     deviceArray.unshift(i);
 }
 console.log(deviceArray);
-
-// var client_up1 = mqtt.connect('mqtt://' + broker, {
-//     'clientId': 'sunic_cly_' + Math.random().toString(16).substr(2, 8),
-//     'deviceId': 'aaa1'
-//     // 'deviceId': deviceArray
-// });
-
-// var client_up2 = mqtt.connect('mqtt://' + broker, {
-//     'clientId': 'sunic_cly_' + Math.random().toString(16).substr(2, 8),
-//     'deviceId': 'aaa2'
-//     // 'deviceId': deviceArray
-// });
-
-// client_up1.on('connect', () => {
-//     console.log(client_up1.options.deviceId);
-//     client_up1.subscribe('cly_0_up');
-
-// });
-// client_up2.on('connect', () => {
-//     console.log(client_up2.options.deviceId);
-// });
-
 
 // 注意nodjs 异步机制, 参见https://blog.csdn.net/fangjian1204/article/details/50585073
 /*
@@ -69,31 +53,22 @@ deviceArray.forEach(function (idx) {
         console.log(client_up.options.clientId + ' connected');
         var topic_up = cly_topic_prefix + client_up.options.deviceId + cly_upload_sufix;
         var topic_down = cly_topic_prefix2 + client_up.options.deviceId + cly_download_sufix;
-        // console.log('topic:' + topic_up);
-        // client_up.subscribe(topic_up);
         client_up.subscribe(topic_down);
-        // 发送上线数据指令
+        // 链接成功后发送上线数据指令
         console.log('client_up.options.deviceId:' + client_up.options.deviceId)
         client_up.publish(topic_up, '#SERVER,1,' + client_up.options.deviceId);
     });
     // 客户端链接接收数据后处理
     client_up.on('message', (topic, buffer) => {
-        // console.log('topic: ' + topic);
         console.log(common.getCurrentTime('YYYY-MM-DD HH:mm:ss.SSS') + '>>' + topic + ';' + buffer.toString());
 
         var resp = buffer.toString();
-        var cly_data = ['#UPLOAD,3,1,75,001,95.20000,青岛欧森测试,12345678,12345678,青岛港,柴油,0#,青,',
-            '#UPLOAD,3,2,75,欧森海事技术服务有限公司,1,青岛欧森,1,标准样品,xxx,201,',
-            '#UPLOAD,3,3,56,2019/05/07 16:01,青岛欧森海事技术服务有限公司'
-        ];
         var data_len = cly_data.length;
         if (resp.startsWith('#SERVER,1,')) {
             var _topicUp = topic.replace('down', 'up');
             client_up.publish(_topicUp, cly_data[0].replace("青岛欧森测试", _topicUp + "青岛欧森测试"));
-            console.log(93 + cly_data[0].replace("青岛欧森测试", _topicUp + "青岛欧森测试"));
+            console.log(0 + cly_data[0].replace("青岛欧森测试", _topicUp + "青岛欧森测试"));
         }
-        // else if (resp.startsWith('#UPLOAD,')) {
-        // } 
         else if (resp.startsWith('#SERVER,2,')) {
             var _topicUp = topic.replace('down', 'up');
             var _pkgIdx = parseInt(resp.substr(resp.lastIndexOf(',') + 1));
